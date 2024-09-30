d6fbef8c34853566ef81d91e10e88e16a62751ff
AlyssonTrugilio <alyson.silvatrugilio121@gmail.com>
2024-09-30 17:52:43 -0300

    feat: criando o post com comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { Header } from './components/Header';
-import Post from './Post';
-import './global.css'
-function App() {
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
 
+import styles from "./App.module.css";
+
+import "./global.css";
+
+function App() {
   return (
     <div>
-   <Header/>
-    </div>  
-  )
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post />
+          <br />
+          <Post />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Post.jsx
+++
@@ -1,9 +0,0 @@
-function Post(){
-  return (
-
-    <h2>Post3</h2>
-  )
-  
-}
-
-export default Post
